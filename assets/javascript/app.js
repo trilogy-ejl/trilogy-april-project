8bd3fb28d2143514f90cab3a09ed17b856f18514
jimmy <rucker>
2019-04-30 19:12:15 -0500

    Function added to app file for events

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,70 @@
 function weather() {
 
-    var location = document.getElementById("location");
-    var apiKey = 'd53c269236c7ba100d14569e5957fee0';
-    var url = 'https://api.forecast.io/forecast/';
-  
-    navigator.geolocation.getCurrentPosition(success, error);
-  
-    function success(position) {
-      latitude = position.coords.latitude;
-      longitude = position.coords.longitude;
-  
-    //   location.innerHTML = 'Latitude is ' + latitude + '° <br> Longitude is ' + longitude + '°';
-  
-       $.getJSON(url + apiKey + "/" + latitude + "," + longitude + "?callback=?", function(data) {
-        $('#temp').html(data.currently.temperature + '° F');
-        $('#minutely').html(data.minutely.summary);
-      });
-    }
-  
-    function error() {
-      location.innerHTML = "Unable to retrieve your location, please allow location usage.";
-    }
-  
-    // location.innerHTML = "Locating...";
-  }
-  
-  weather();
+  var location = document.getElementById("location");
+  var apiKey = 'd53c269236c7ba100d14569e5957fee0';
+  var url = 'https://api.forecast.io/forecast/';
 
-!function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = 'https://weatherwidget.io/js/widget.min.js'; fjs.parentNode.insertBefore(js, fjs); } }(document, 'script', 'weatherwidget-io-js');
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+    //   location.innerHTML = 'Latitude is ' + latitude + '° <br> Longitude is ' + longitude + '°';
+
+    $.getJSON(url + apiKey + "/" + latitude + "," + longitude + "?callback=?", function (data) {
+      $('#temp').html(data.currently.temperature + '° F');
+      $('#minutely').html(data.minutely.summary);
+    });
+  }
+
+  function error() {
+    location.innerHTML = "Unable to retrieve your location, please allow location usage.";
+  }
+
+  // location.innerHTML = "Locating...";
+}
+
+function events() {
+
+  var queryURL =
+    'https://cors-anywhere.herokuapp.com/https://www.eventbriteapi.com/v3/events/search?location.longitude=-87.623177&location.latitude=41.881832&expand=venue'
+  $.ajax({
+    url: queryURL,
+    method: "GET", // Headers 
+    headers: {
+      "Authorization": "Bearer EAYDY5OXKROACFETSIVZ",
+    }
+  }).then(function (response) {
+    console.log(response.events);
+    for (var i = 0; i < response.events.length; i++) {
+      console.log('*******DESCRIPTION', response.events[i].description.text);
+      console.log('**********NAME', response.events[i].name.text);
+      var descrip = $("<p>").text(response.events[i].description.text);
+      var name = $("<h1>").text(response.events[i].name.text);
+      $("#liveInformation").append(name, descrip);
+      // $('#output').text(response.events[1]);
+    }
+
+
+
+  })
+
+
+
+
+}
+
+
+weather();
+events();
+
+! function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (!d.getElementById(id)) {
+    js = d.createElement(s);
+    js.id = id;
+    js.src = 'https://weatherwidget.io/js/widget.min.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }
+}(document, 'script', 'weatherwidget-io-js');
