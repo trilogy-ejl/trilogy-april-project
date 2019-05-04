b2a38539487d376e9c7792b53e3a50dac07e08c4
Eddie A. Linares <eddie.a.linares@gmail.com>
2019-05-04 00:43:27 -0500

    Fixed some CSS and Buttons for pages

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -110,3 +110,14 @@ $(document).ready(function () {
   });
 
 });
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(3000)
+    .next()
+    .fadeIn(3000)
+    .end()
+    .appendTo('#slideshow');
+}, 6000);
